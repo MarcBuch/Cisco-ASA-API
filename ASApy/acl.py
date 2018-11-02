7b1cce8f1ce770e23dd9a0b6be0a9b61d1632567
MarcBuch <37442961+MarcBuch@users.noreply.github.com>
2018-11-02 16:00:51 +0100

    Add files via upload

---
+++ ASApy/acl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding : utf-8
+
+'''
+Created on 12.10.2018
+@author: MBU
+'''
+
+
+
+if __name__ == '__main__':
+    pass
+
+import json
+
+with open('ACL.json', 'r') as f:
+    ACL_dict = json.load(f)
